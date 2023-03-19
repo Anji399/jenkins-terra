fbbb7ff344c42d1b0ec2da1f34df6c2e6ab0752e
Anji399 <58341395+Anji399@users.noreply.github.com>
2023-03-19 08:15:47 +0530

    Update jen.sh

--- jen.sh
+++ jen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo amazon-linux-extras install epel -y
+sudo yum update -y
 sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install maven -y
 sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
